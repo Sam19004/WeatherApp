efacaa369ac212949474abefbd11e09d53d0188d
Samarth Prajapati <102741553+Sam19004@users.noreply.github.com>
2022-09-23 15:54:14 +0530

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const getApiOption = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key':'8ba251d3a2msh9e8286a48a3c67ap104df6jsn760641671750',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL='https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const WEATHER_API_URL='https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY='870cb99c51a3c1849219b3a65aaef906';
+
